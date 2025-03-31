e5af28d7af9ded031ce02ab6afb473aa939bc680
theobassan <theobassan@gmail.com>
2025-03-31 11:49:40 -0300

    feat(actions): add custom actions and workflows

--- src/commitizen/index.ts
+++ src/commitizen/index.ts
@@ -1,5 +1,5 @@
 import commitizenPrompter from './prompter.js';
-import configLoader from '../configLoader/configLoader.js';
+import configLoader from '../config-loader/configLoader.js';
 import CommitWizardOptions from '../types/CommitWizardOptions.js';
 
 const loadedConfig: CommitWizardOptions = configLoader();
--- src/commitlint/index.ts
+++ src/commitlint/index.ts
@@ -1,7 +1,7 @@
 import { UserConfig } from '@commitlint/types';
 
 import commitlintUserConfig from './userConfig.js';
-import configLoader from '../configLoader/configLoader.js';
+import configLoader from '../config-loader/configLoader.js';
 import CommitWizardOptions from '../types/CommitWizardOptions.js';
 
 const loadedConfig: CommitWizardOptions = configLoader();
