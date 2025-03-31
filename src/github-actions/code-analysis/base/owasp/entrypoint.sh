0a45ebd8b658ea5e947a9d670c061888721dbebb
theobassan <theobassan@gmail.com>
2025-03-31 14:02:05 -0300

    fix(owasp): try dockerfile

---
+++ src/github-actions/code-analysis/base/owasp/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+# Build base command array.
+args=(--project "${INPUT_PROJECT}" \
+      --scan "$GITHUB_WORKSPACE/${INPUT_PATH}" \
+      --format "${INPUT_FORMAT}" \
+      --out "$GITHUB_WORKSPACE/${INPUT_OUT}" \
+      --noupdate \
+      --enableRetired \
+      --failOnCVSS 7)
+
+# If the suppression input is provided, append the flag.
+if [ -n "${INPUT_SUPPRESSION_FILE}" ]; then
+    args+=(--suppression "${INPUT_SUPPRESSION_FILE}")
+fi
+
+# Execute OWASP Dependency-Check with the built arguments.
+exec dependency-check "${args[@]}"
